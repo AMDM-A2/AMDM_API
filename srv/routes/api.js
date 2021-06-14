6a11d55df772e30f432f982c01e436a01fda82e2
bizouarn <aymeric.bizouarn@hotmail.fr>
2021-06-14 16:01:08 +0200

    api : add sort by data

--- srv/routes/api.js
+++ srv/routes/api.js
@@ -1,19 +1,19 @@
-var db = require('./database.js')
-var express = require('express')
+const db = require('./database.js')
+const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.json({ yes: 'oui' })
-})
 /* GET API page. */
 app.get('/data', function (req, res, next) {
-  var sql = 'select * from Gestion'
-  var params = []
+  const sql = 'select * from Gestion'
+  const params = []
   db.all(sql, params, (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message })
       return
     }
+    rows.sort(function (a, b) {
+      return a.hour.localeCompare(b.hour)
+    })
     res.json(rows)
   })
 })
--- srv/routes/database.js
+++ srv/routes/database.js
@@ -1,6 +1,6 @@
-var sqlite3 = require('sqlite3').verbose()
+const sqlite3 = require('sqlite3').verbose()
 
-const DBSOURCE = 'db.sqlite'
+const DBSOURCE = 'db.db'
 
 const db = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) {
