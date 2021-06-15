d7086f25a476152d762bfdc99a0cb1e92f46c5ee
kernoeb <kernoeb@protonmail.com>
2021-06-15 12:05:44 +0200

    fix: update ui and api

--- src/components/Datas.vue
+++ src/components/Datas.vue
@@ -1,16 +1,124 @@
 <template>
   <v-container>
-    <div class="d-flex justify-center flex-column align-content-center">
-        <v-data-iterator
-          :items="items"
-        />
+    <v-data-iterator
+      :items="items"
+      :items-per-page="5"
+      :loading="loading"
+      loading-text="Chargement des données"
+      no-data-text="Aucune donnée"
+      no-results-text="Aucun résultat"
+    >
+      <template v-slot:header>
+        <v-toolbar
+          class="mb-4"
+          color="#8e0088"
+          dark
+          flat
+        >
+          <v-toolbar-title>Liste de lots</v-toolbar-title>
+        </v-toolbar>
+      </template>
 
-        <!-- <v-card max-width="900">
-          <div id="chart">
-            <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
-          </div>
-        </v-card> -->
-    </div>
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="item in props.items.filter(v => v.lotId)"
+            :key="item.name"
+            cols="12"
+          >
+            <v-card outlined>
+              <v-card-title class="subheading font-weight-bold">
+                Lot n°{{ item.lotId }}
+              </v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Alertes / Défaut :</v-list-item-content>
+                  <v-list-item-content v-if="item.alert" class="align-end">
+                    {{ item.alert }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="ml-4 mr-4"/>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Produit A :</v-list-item-content>
+                  <v-list-item-content v-if="item.productA" class="align-end">
+                    {{ item.productA }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Produit B :</v-list-item-content>
+                  <v-list-item-content v-if="item.productB" class="align-end">
+                    {{ item.productB }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Produit C :</v-list-item-content>
+                  <v-list-item-content v-if="item.productC" class="align-end">
+                    {{ item.productC }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="ml-4 mr-4"/>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Total Produit A :</v-list-item-content>
+                  <v-list-item-content v-if="item.totalProductA" class="align-end">
+                    {{ item.totalProductA }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Total Produit B :</v-list-item-content>
+                  <v-list-item-content v-if="item.totalProductB" class="align-end">
+                    {{ item.totalProductB }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content class="font-weight-bold">Total Produit C :</v-list-item-content>
+                  <v-list-item-content v-if="item.totalProductC" class="align-end">
+                    {{ item.totalProductC }}
+                  </v-list-item-content>
+                  <v-list-item-content v-else class="align-end grey--text">
+                    Aucune donnée
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-iterator>
+
+    <!-- <v-card max-width="900">
+      <div id="chart">
+        <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+      </div>
+    </v-card> -->
   </v-container>
 </template>
 
@@ -20,9 +128,10 @@ export default {
   data () {
     return {
       // Iterator
-      items: [],
+      loading: true,
+      items: []
 
-      // Chart
+      /* // Chart
       series: [{
         name: 'Net Profit',
         data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
@@ -71,22 +180,15 @@ export default {
             }
           }
         }
-      }
+      } */
     }
   },
   created () {
-    this.axios.get('http://localhost:3000/api/v1/data').then(v => {
-      this.items = [{
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%'
-      }]
-      console.log(this.items)
+    this.axios.get('/api/v1/data').then(v => {
+      this.items = v.data
+      this.loading = false
+    }).catch(() => {
+      this.loading = false
     })
   }
 }
--- srv/routes/api.js
+++ srv/routes/api.js
@@ -2,29 +2,25 @@ const db = require('./database.js')
 const express = require('express')
 const app = express()
 
-function getLastProduct(rows, product) {
+function getLastProduct (rows, product) {
   const tmp = rows.filter(v => v && v.identifiant === product).sort((a, b) => a.hour.localeCompare(b.hour))
-  return tmp && tmp.length && tmp[tmp.length - 1].valeur || undefined
+  return (tmp && tmp.length && tmp[tmp.length - 1].valeur) || undefined
 }
 
-function getLastAlert(rows) {
+function getLastAlert (rows) {
   const tmp = rows.filter(v => v && v.identifiant === 'alert').sort((a, b) => a.hour.localeCompare(b.hour))
-  return tmp && tmp.length && tmp[tmp.length - 1].valeur || undefined
+  return (tmp && tmp.length && tmp[tmp.length - 1].valeur) || undefined
 }
 
-function getProductSum(rows, product) {
+function getProductSum (rows, product) {
   const tmp = rows.filter(v => v && v.identifiant === product).reduce((v, i) => v + i.valeur, 0)
   return tmp || undefined
 }
 
-function getBundleNumber(rows) {
-  return rows && rows[0].numeroLot || undefined
-}
-
 /* GET API page. */
 app.get('/data', function (req, res, next) {
-  const sql = 'SELECT identifiant, numeroLot, hour, valeur FROM Gestion WHERE numeroLot = (SELECT MAX(numeroLot) FROM Gestion)';
-  //SELECT identifiant,valeur FROM Gestion WHERE identifiant LIKE "produit%" GROUP BY identifiant HAVING MAX(hour);
+  const sql = 'SELECT * FROM Gestion'
+  // SELECT identifiant,valeur FROM Gestion WHERE identifiant LIKE "produit%" GROUP BY identifiant HAVING MAX(hour);
 
   db.all(sql, [], (err, rows) => {
     if (err) {
@@ -32,18 +28,30 @@ app.get('/data', function (req, res, next) {
       return
     }
 
+    const lots = {}
+    for (const row of rows) {
+      const tmp = JSON.parse(JSON.stringify(row))
+      delete tmp.id
+      delete tmp.numeroLot
+      if (lots[row.numeroLot]) lots[row.numeroLot].push(tmp)
+      else lots[row.numeroLot] = [tmp]
+    }
 
-    res.json([{
-      name: 'Test',
-      productA: getLastProduct(rows, "produitA"), 
-      productB: getLastProduct(rows, "produitB"), 
-      productC: getLastProduct(rows, "produitC"),
-      bundleNumber:  getBundleNumber(rows),
-      alert: getLastAlert(rows),
-      totalProductA: getProductSum(rows, "produitA"), 
-      totalProductB: getProductSum(rows, "produitB"), 
-      totalProductC: getProductSum(rows, "produitC")
-    }])
+    const finalLots = []
+    for (const r in lots) {
+      finalLots.push({
+        lotId: r,
+        alert: getLastAlert(lots[r]),
+        productA: getLastProduct(lots[r], 'produitA'),
+        productB: getLastProduct(lots[r], 'produitB'),
+        productC: getLastProduct(lots[r], 'produitC'),
+        totalProductA: getProductSum(lots[r], 'produitA'),
+        totalProductB: getProductSum(lots[r], 'produitB'),
+        totalProductC: getProductSum(lots[r], 'produitC')
+      })
+    }
+
+    res.json(finalLots.sort((a, b) => b.lotId - a.lotId))
   })
 })
 
