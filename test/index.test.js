ad16f9345212a3a5dddc30637463912a11b07bee
kernoeb <kernoeb@protonmail.com>
2021-06-17 09:38:30 +0200

    fix: tests

--- test/index.test.js
+++ test/index.test.js
@@ -35,7 +35,8 @@ describe('Test principal', function () {
     it('get data verify body', function () {
       request.get('http://localhost:3000/api/v1/data', (err, response, body) => {
         if (err) assert.fail('Error while getting API')
-        assert.ok((String)(body).includes('{"lotId":"12912","data":[{"typeProduit":"Alert","heure":"2021-06-11 23:09:52","valeur":35,"sum":956},{"typeProduit":"Produit A","heure":"2021-06-11 23:53:46","valeur":31,"sum":705},{"typeProduit":"Produit B","heure":"2021-06-11 22:56:42","valeur":43,"sum":611},{"typeProduit":"Produit C","heure":"2021-06-11 22:28:21","valeur":18,"sum":594}]}'))
+        assert.strictEqual(JSON.parse(body)[0].lotId, '12912')
+        assert.strictEqual(JSON.parse(body)[0].data[1].valeur, 31)
       })
     })
   })
