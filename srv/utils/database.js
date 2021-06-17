194b4a9c94e4f37a22217456db6967f5c2af6b5c
bizouarn <aymeric.bizouarn@hotmail.fr>
2021-06-17 10:50:38 +0200

    change database create script

--- srv/utils/database.js
+++ srv/utils/database.js
@@ -9,37 +9,31 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
     throw err
   } else {
     // create table if not exist
-    db.run(`CREATE TABLE "TypesProduits" (
-        "id"    INTEGER,
-        "libelle"    TEXT UNIQUE,
-        PRIMARY KEY("id")
-    );
+    db.run(`
+            CREATE TABLE "Lots" (
+                "id"    INTEGER,
+                "description"    TEXT,
+                PRIMARY KEY("id")
+            );
 
-    CREATE TABLE "Alertes" (
-        "id"    INTEGER,
-        "numeroLot"    INTEGER NOT NULL,
-        "heure"    datetime NOT NULL,
-        "valeur"    INTEGER,
-        PRIMARY KEY("id"),
-        FOREIGN KEY("numeroLot") REFERENCES "Lots"("id")
-    );
+            CREATE TABLE "Alertes" (
+                "id"    INTEGER,
+                "idLot"    INTEGER NOT NULL,
+                "heure"    datetime NOT NULL,
+                "description"    TEXT,
+                PRIMARY KEY("id"),
+                FOREIGN KEY("idLot") REFERENCES "Lots"("id")
+            );
 
-    CREATE TABLE "Lots" (
-        "id"    INTEGER,
-        "description"    TEXT,
-        PRIMARY KEY("id")
-    );
-
-    CREATE TABLE "Produits" (
-        "id"    INTEGER,
-        "typeProduit"    INTEGER NOT NULL,
-        "numeroLot"    INTEGER NOT NULL,
-        "heure"    datetime NOT NULL,
-        "valeur"    INTEGER NOT NULL,
-        FOREIGN KEY("typeProduit") REFERENCES TypesProduits(id),
-        FOREIGN KEY("numeroLot") REFERENCES Lots(id),
-        PRIMARY KEY("id")
-    );`,
+            CREATE TABLE "Capteurs" (
+                "id"    INTEGER,
+                "idLot"    INTEGER NOT NULL,
+                "heure"    datetime NOT NULL,
+                "libelle"    TEXT NOT NULL,
+                "valeur"    INTEGER NOT NULL,
+                PRIMARY KEY("id"),
+                FOREIGN KEY("idLot") REFERENCES Lots(id)
+            );`,
     // eslint-disable-next-line node/handle-callback-err
     (err) => {})
   }
