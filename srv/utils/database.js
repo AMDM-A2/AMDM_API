17c4a5fd433903af1018cf6afc4f861b03aae30e
bizouarn <aymeric.bizouarn@hotmail.fr>
2021-06-17 09:40:51 +0200

    change database create script

--- srv/utils/database.js
+++ srv/utils/database.js
@@ -9,15 +9,37 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
     throw err
   } else {
     // create table if not exist
-    db.run(`CREATE TABLE "Gestion"
-            (
-              "id"          INTEGER NOT NULL UNIQUE,
-              "identifiant" TEXT    NOT NULL,
-              "numeroLot"   INTEGER NOT NULL,
-              "hour"        TEXT    NOT NULL,
-              "valeur"      INTEGER NOT NULL,
-              PRIMARY KEY ("id" AUTOINCREMENT)
-            );`,
+    db.run(`CREATE TABLE "TypesProduits" (
+        "id"    INTEGER,
+        "libelle"    TEXT UNIQUE,
+        PRIMARY KEY("id")
+    );
+
+    CREATE TABLE "Alertes" (
+        "id"    INTEGER,
+        "numeroLot"    INTEGER NOT NULL,
+        "heure"    datetime NOT NULL,
+        "valeur"    INTEGER,
+        PRIMARY KEY("id"),
+        FOREIGN KEY("numeroLot") REFERENCES "Lots"("id")
+    );
+
+    CREATE TABLE "Lots" (
+        "id"    INTEGER,
+        "description"    TEXT,
+        PRIMARY KEY("id")
+    );
+
+    CREATE TABLE "Produits" (
+        "id"    INTEGER,
+        "typeProduit"    INTEGER NOT NULL,
+        "numeroLot"    INTEGER NOT NULL,
+        "heure"    datetime NOT NULL,
+        "valeur"    INTEGER NOT NULL,
+        FOREIGN KEY("typeProduit") REFERENCES TypesProduits(id),
+        FOREIGN KEY("numeroLot") REFERENCES Lots(id),
+        PRIMARY KEY("id")
+    );`,
     // eslint-disable-next-line node/handle-callback-err
     (err) => {})
   }
