ae1e4a9c50041fc549f7fcb80e034296b9cb733a
Martin Barré <barre.e1802464@etud.univ-ubs.fr>
2021-06-23 11:11:23 +0200

    Add POST and PUT routes for Lot

--- srv/index.js
+++ srv/index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 
 export default (app) => {
   app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
   app.use(cors())
 
   app.use('/api/v1', require('./routes/api'))
+  app.use('/api/v1/lot', require('./routes/lot'))
 }
---
+++ srv/routes/lot.js
@@ -0,0 +1,39 @@
+const db = require('../utils/database.js')
+const express = require('express')
+const app = express()
+
+/* API : Lot : POST */
+app.post('/', function (req, res, next) {
+  const id = req.body.id
+  const description = req.body.description
+
+  if (!id) return res.status(400).json({ error: 'Bad Request - The parameter \'id\' is required' })
+  if (!description) return res.status(400).json({ error: 'Bad Request - The parameter \'description\' is required' })
+
+  const array = [id, description]
+  const sql = 'INSERT INTO Lots (id, description) VALUES (?, ?)'
+
+  db.run(sql, array, (err) => {
+    if (err) return res.status(400).json({ error: err })
+    return res.status(201).json()
+  })
+})
+
+/* API : Lot : PUT */
+app.put('/:id', function (req, res, next) {
+  const id = req.params.id
+  const description = req.body.description
+
+  if (!id) return res.status(400).json({ error: 'Bad Request - The parameter \'id\' is required' })
+  if (!description) return res.status(400).json({ error: 'Bad Request - The parameter \'description\' is required' })
+
+  const array = [id, description]
+  const sql = 'UPDATE Lots SET description = ? WHERE id = ?'
+
+  db.run(sql, array, (err) => {
+    if (err) return res.status(400).json({ error: err })
+    return res.status(200).json()
+  })
+})
+
+module.exports = app
